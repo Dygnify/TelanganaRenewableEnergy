bd84f7d20a5e553124c4914ea331ef1131439683
MohammadRakib1e18 <trustedrakib@gmail.com>
2023-04-14 21:46:37 +0600

    fix project page and carbon credit

--- src/components/cards/CarbonCredit.js
+++ src/components/cards/CarbonCredit.js
@@ -7,42 +7,44 @@ import ConvertCarbonCredit from "./ConvertCarbonCredit";
 import CreditConversionMsg from "./CreditConversionMsg";
 
 const CarbonCredit = () => {
-    const [carbonCredit, setCarbonCredit] = useState(50);
+	const [carbonCredit, setCarbonCredit] = useState(5000);
 	const [isConvertCarbonCredit, setIsConvertCarbonCredit] = useState(false);
-    const [isCreditConversionMsg, setIsCreditConversionMsg ] = useState(false);
+	const [isCreditConversionMsg, setIsCreditConversionMsg] = useState(false);
 	return (
 		<div>
-			<div className="relative overflow-hidden border-2 border-[#caedf7] w-2/3 p-2 mx-auto flex  items-center justify-between rounded-md box-shadow2 carbon-credit-bg">
-				<div className=" flex items-center justify-center gap-4 p-4">
-					<div className="w-[100px] h-[100px] border border-[#0dacac] rounded-full scale-[1.15]  p-[2px] flex justify-center items-center mix-blend-multiply">
-						<img
-							src={carbon_credit}
-							alt=""
-							className="mix-blend-multiply w-full h-full rounded-md "
-						/>
+			<div className="border-2 border-[#caedf7]  p-4 mx-auto rounded-md box-shadow2 carbon-credit-bg">
+				<div className="w-2/3 mx-auto flex  items-center justify-between rounded-md box-shadow6 bg-[#ffffff] carbon-border-style">
+					<div className=" flex items-center justify-center gap-4 p-2">
+						<div className="w-[100px] h-[100px] rounded-full scale-[1.15]  p-[2px] flex justify-center items-center mix-blend-multiply">
+							<img
+								src={carbon_credit}
+								alt=""
+								className="mix-blend-multiply w-full h-full rounded-md hover:scale-[1.25] transition ease-in-out duration-1000"
+							/>
+						</div>
+						<div className=" font-semibold text-xl text-font-semibold text-slate-500 drop-shadow-lg">
+							<p className="text-[#0dacac] text-[28px] font-bold flex justify-center">
+								{carbonCredit} (~
+								{carbonCredit * 40}
+								<div className="">
+									<img
+										src={rupeeIcon}
+										alt=""
+										className=" w-5 h-5 ml-[1px] -mt-1 "
+									/>
+								</div>
+								)
+							</p>
+						</div>
 					</div>
-					<div className=" font-semibold text-xl text-font-semibold text-slate-500 drop-shadow-lg">
-						<p className="text-[#0dacac] text-[28px] font-bold flex justify-center">
-							{carbonCredit} (~
-							{carbonCredit * 40}
-							<div className="">
-								<img
-									src={rupeeIcon}
-									alt=""
-									className=" w-5 h-5 ml-[1px] -mt-1 "
-								/>
-							</div>
-							)
-						</p>
+					<div className="w-2/5 relative px-2">
+						<Button
+							className="w-full carbon-credit-button"
+							onClick={() => setIsConvertCarbonCredit(true)}
+						>
+							Tokenize
+						</Button>
 					</div>
-				</div>
-				<div className="w-2/5 relative px-2">
-					<Button
-						className="w-full carbon-credit-button"
-						onClick={() => setIsConvertCarbonCredit(true)}
-					>
-						Tokenize
-					</Button>
 				</div>
 			</div>
 			<div
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,6 +4,147 @@ import "./Projects.css";
 import CarbonCredit from "../../components/cards/CarbonCredit";
 
 const Projects = () => {
+	const projectCards = [
+		{
+			title: "Project Description",
+			details:
+				"Usage of solar pumps by smallholder farmers in Telangana for irrigation",
+		},
+		{
+			title: "Project Category",
+			details: "The Demo Information",
+		},
+		{
+			title: "Project Files",
+			details: "No file available",
+		},
+		{
+			title: "Location",
+			details: "2-3 districts with boundary touching each other",
+		},
+		{
+			title: "Type",
+			details: "Renewable Energy (Solar)",
+		},
+		{
+			title: "Authorized Project Contact",
+			details: "Dygnify",
+		},
+		{
+			title: "Project Proponent",
+			details: "992229753",
+		},
+	];
+	const projectInfo = [
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2021",
+			},
+			{
+				title: "CoU's Available",
+				details: "85",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2022",
+			},
+			{
+				title: "CoU's Available",
+				details: "105",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2023",
+			},
+			{
+				title: "CoU's Available",
+				details: "98",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2024",
+			},
+			{
+				title: "CoU's Available",
+				details: "134",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2025",
+			},
+			{
+				title: "CoU's Available",
+				details: "75",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+		[
+			{
+				title: "Serial Number",
+				details: "0001-005005-005099-UCR-CoU-IN-008-01012018-31122018",
+			},
+			{
+				title: "Vintage Year",
+				details: "2026",
+			},
+			{
+				title: "CoU's Available",
+				details: "110",
+			},
+			{
+				title: "Status",
+				details: "Registered (Others)",
+			},
+		],
+	];
 	return (
 		<div>
 			<h2 className="text-[1.4375rem] lg:text-[2rem] font-semibold text-slate-500  drop-shadow-lg px-2">
@@ -13,65 +154,17 @@ const Projects = () => {
 			<div></div>
 			<div className="flex justify-between gap-4">
 				<div className="w-1/2 grid grid-cols-2 gap-4">
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
+					{projectCards.map((card, i) => (
+						<div
+							key={i}
+							className="box-shadow6   bg-[#F3FFFE] flex flex-col justify-center  items-center rounded-lg text-center min-h-[100px]"
+						>
+							<p className="font-bold text-slate-600">
+								{card.title}
+							</p>
+							<p className=" text-slate-500 ">{card.details}</p>
+						</div>
+					))}
 				</div>
 				<div className="w-1/2">
 					<img
@@ -83,149 +176,34 @@ const Projects = () => {
 			</div>
 			<br />
 			<div className="bg-[#eefdfb] p-4 border border-[#caedf7] rounded-lg">
-				<section className="grid grid-cols-3 gap-4 mt-6">
-					<div className=" flex flex-col justify-center  rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
+				{projectInfo.map((info, i) => (
+					<div key={i}>
+						<section className="grid grid-cols-3 gap-4 mt-6">
+							{info.map((item, i) => (
+								<div
+									key={i}
+									className=" flex flex-col justify-center  rounded-lg"
+								>
+									<p className="font-bold text-slate-600">
+										{item.title}
+									</p>
+									<p className=" text-slate-500" title={item.details}>
+										{item.details.length>40 ? item.details.slice(0,40)+'...':item.details}
+									</p>
+								</div>
+							))}
+						</section>
+						{i + 1 < projectInfo.length ? (
+							<div className="divider "></div>
+						) : (
+							<span></span>
+						)}
 					</div>
-					<div className="  flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-				</section>
-				<div className="divider "></div>
-				<section className="grid grid-cols-3 gap-4 mt-6">
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="  flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-				</section>
-				<div className="divider"></div>
-				<section className="grid grid-cols-3 gap-4 mt-6">
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="  flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-				</section>
-				<div className="divider"></div>
-				<section className="grid grid-cols-3 gap-4 mt-6">
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className="  flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-					<div className=" flex flex-col justify-center   rounded-lg ">
-						<p className="font-bold text-slate-600">
-							Project Description
-						</p>
-						<p className="text-[18px] text-slate-500">
-							The Demo Information
-						</p>
-					</div>
-				</section>
+				))}
 			</div>
 			<div className="px-3 py-2 flex justify-between items-center bg-slate-700 text-slate-50 text-[18px] rounded-md">
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-				<p>190,405</p>
+				<p className="font-bold">190,405</p>
 			</div>
 			<br />
 			<CarbonCredit />
